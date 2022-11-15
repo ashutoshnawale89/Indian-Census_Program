de0f0c4638700114dc026cfe2066aef1f65dd1d1
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-11-15 21:02:00 +0530

    Added: Print The Welcome Message

---
+++ src/main/java/com/program/CensusAnalyser.java
@@ -0,0 +1,18 @@
+package com.program;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+public class CensusAnalyser {
+	
+	public static void main(String[] args) {
+		    System.out.println("Welcome To  Indian Census Program");
+	}
+
+}
